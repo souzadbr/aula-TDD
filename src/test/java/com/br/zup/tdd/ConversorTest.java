d449ba10309c809a60d5e7fa19e35cc4f41da4be
Debora Souza <deboral.p@hotmail.com>
2021-12-23 09:21:01 -0300

    Finalizar teste do metodo de converter numeros

--- src/test/java/com/br/zup/tdd/ConversorTest.java
+++ src/test/java/com/br/zup/tdd/ConversorTest.java
@@ -1,12 +1,16 @@
 package com.br.zup.tdd;
 
 import org.junit.Assert;
+import org.junit.Test;
 
-public class Conversor {
+public class ConversorTest {
 
+    @Test
     public void testarMetodoDeConversaoNumerosRomanos(){
         String resultado = new Conversor().converterParaNumeroRoman(5478);
 
         Assert.assertEquals("VCDLXXVIII",resultado);
     }
+
+
 }
